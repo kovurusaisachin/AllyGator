d33842d94d808e7fd43fc64b76e562bc15dd3a8d
Sai Sachin Kovuru <saisachinkovuru@Sais-MacBook-Pro.local>
2022-02-02 02:19:33 -0500

    analyic card component created

--- Frontend/allygator_frontend/pages/dashboard/index.tsx
+++ Frontend/allygator_frontend/pages/dashboard/index.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import Sidebar from "../../components/sidebar/sidebar"
+import AnalyticCard from "../../components/analyticCard/analyticCard"
 export default function dashboard () {
   return (
     <>
       {/* sidebar */}
       <Sidebar />
+      <AnalyticCard />
     </>
   )
 }
