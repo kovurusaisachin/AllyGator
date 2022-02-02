5ac8ed279e6b4201d5abbd588e943d688af11764
prashantkapri7@gmail.com <prashantkapri7@gmail.com>
2022-02-01 22:39:54 -0500

    new structure added

--- Frontend/allygator_frontend/pages/login.tsx
+++ Frontend/allygator_frontend/pages/login.tsx
@@ -21,13 +21,12 @@ export default function Login() {
               <div className="mx-auto max-w-lg">
                 <div className="py-2">
                   <span className="px-1 text-sm text-gray-600">Username</span>
-                  <input placeholder type="text" className="text-md block px-3 py-2  rounded-lg w-full 
-          bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none" />
+                  <input placeholder="abc@xyz.com" type="text" className="text-md block px-3 py-2  rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none" />
                 </div>
                 <div className="py-2" x-data="{ show: true }">
                   <span className="px-1 text-sm text-gray-600">Password</span>
                   <div className="relative">
-                    <input placeholder type="show ? 'password' : 'text'" className="text-md block px-3 py-2 rounded-lg w-full 
+                    <input placeholder="******" type="show ? 'password' : 'text'" className="text-md block px-3 py-2 rounded-lg w-full 
           bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md
           focus:placeholder-gray-500
           focus:bg-white 
--- Frontend/allygator_frontend/pages/signup.tsx
+++ Frontend/allygator_frontend/pages/signup.tsx
@@ -88,7 +88,7 @@ export default function Signup() {
                 <textarea
                   className="block w-full px-4 py-3 mt-1 mb-3 text-gray-700 bg-gray-200 border border-gray-200 rounded form-textarea focus:outline-none"
                   rows={4}
-                  placeholder
+                  placeholder=" x sw, y street"
                   defaultValue={""}
                 />
               </label>
@@ -145,7 +145,7 @@ export default function Signup() {
                     className="block w-full px-4 py-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500"
                     id="grid-zip"
                     type="text"
-                    placeholder={90210}
+                    placeholder={"90210"}
                   />
                 </div>
               </div>
