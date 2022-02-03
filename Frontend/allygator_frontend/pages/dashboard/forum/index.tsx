ea160038a587995c927dc303429cfc931db65625
Noopur Thanvi <noopurthanvi@Noopurs-MacBook-Air.local>
2022-02-03 17:40:34 -0500

    forum component added

--- Frontend/allygator_frontend/pages/dashboard/forum/index.tsx
+++ Frontend/allygator_frontend/pages/dashboard/forum/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Sidebar from '../../../components/sidebar/sidebar'
+import ForumCard from '../../../components/forumCard'
 // import ResetPass from './ResetPassword'
 function Forum() {
     return(
         <div className="flex flex-col-2">
             <Sidebar />
+            <Forum />
         </div>
     )
 }
