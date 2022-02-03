3cc039bd39822856703222d82506f8c1429c4b59
Noopur Thanvi <noopurthanvi@Noopurs-MacBook-Air.local>
2022-02-03 17:33:41 -0500

    forum page added

---
+++ Frontend/allygator_frontend/pages/dashboard/forum/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Sidebar from '../../../components/sidebar/sidebar'
+// import ResetPass from './ResetPassword'
+function Forum() {
+    return(
+        <div className="flex flex-col-2">
+            <Sidebar />
+        </div>
+    )
+}
+export default Forum
