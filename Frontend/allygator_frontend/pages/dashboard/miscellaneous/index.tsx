998d993d2a5ee1e54f74f1562afe59768a9a04f2
Noopur Thanvi <noopurthanvi@Noopurs-MacBook-Air.local>
2022-02-03 20:15:44 -0500

    Misc page added

--- Frontend/allygator_frontend/pages/dashboard/miscellaneous/index.tsx
+++ Frontend/allygator_frontend/pages/dashboard/miscellaneous/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import Sidebar from '../../../components/sidebar'
 // import ResetPass from './ResetPassword'
-function Connection() {
+function miscellaneous() {
     return(
         <div className="flex flex-col-2">
             <Sidebar />
             <h2>More Content Coming Soon!!!</h2>
             
-            
         </div>
     )
 }
-export default Connection
+export default miscellaneous
