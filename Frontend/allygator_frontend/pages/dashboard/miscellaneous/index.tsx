6fa3c44e68a6a621529e5b217e932b72a85c5440
Noopur Thanvi <noopurthanvi@Noopurs-MacBook-Air.local>
2022-02-18 19:57:45 -0500

    page design done

--- Frontend/allygator_frontend/pages/dashboard/miscellaneous/index.tsx
+++ Frontend/allygator_frontend/pages/dashboard/miscellaneous/index.tsx
@@ -1,13 +1,140 @@
-import React from 'react'
-import Sidebar from '../../../components/sidebar'
+import React from "react";
+import Sidebar from "../../../components/sidebar";
 // import ResetPass from './ResetPassword'
 function miscellaneous() {
-    return(
-        <div className="flex flex-col-2">
-            <Sidebar />
-            <h2>More Content Coming Soon!!!</h2>
-            
+  return (
+    <div className="flex flex-col-1">
+      <Sidebar />
+      <div className="flex flex-col mx-16 mt-5 grid gap-6 md:grid-cols-2 xl:grid-cols-3">
+        <div className="max-w-xs my-2 mx-2 overflow-hidden bg-white rounded-lg shadow-lg dark:bg-blue-800">
+          <img
+            className="object-cover w-full h-56"
+            src="https://images.unsplash.com/photo-1542156822-6924d1a71ace?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+            alt="avatar"
+          />
+          <div className="py-5 text-center">
+            <a
+              href="#"
+              className="block text-2xl font-bold text-gray-800 dark:text-white"
+            >
+              John Doe
+            </a>
+            <span className="text-sm text-gray-700 dark:text-gray-200">
+              Software Engineer
+            </span>
+          </div>
         </div>
-    )
+        <div className="max-w-xs mx-2 my-2 overflow-hidden bg-white rounded-lg shadow-lg dark:bg-blue-800">
+          <img
+            className="object-cover w-full h-56"
+            src="https://images.unsplash.com/photo-1542156822-6924d1a71ace?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+            alt="avatar"
+          />
+          <div className="py-5 text-center">
+            <a
+              href="#"
+              className="block text-2xl font-bold text-gray-800 dark:text-white"
+            >
+              John Doe
+            </a>
+            <span className="text-sm text-gray-700 dark:text-gray-200">
+              Software Engineer
+            </span>
+          </div>
+        </div>
+        <div className="max-w-xs mx-2 my-2 overflow-hidden bg-white rounded-lg shadow-lg dark:bg-blue-800">
+          <img
+            className="object-cover w-full h-56"
+            src="https://images.unsplash.com/photo-1542156822-6924d1a71ace?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+            alt="avatar"
+          />
+          <div className="py-5 text-center">
+            <a
+              href="#"
+              className="block text-2xl font-bold text-gray-800 dark:text-white"
+            >
+              John Doe
+            </a>
+            <span className="text-sm text-gray-700 dark:text-gray-200">
+              Software Engineer
+            </span>
+          </div>
+        </div>
+        <div className="max-w-xs my-2 mx-2 overflow-hidden bg-white rounded-lg shadow-lg dark:bg-blue-800">
+          <img
+            className="object-cover w-full h-56"
+            src="https://images.unsplash.com/photo-1542156822-6924d1a71ace?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+            alt="avatar"
+          />
+          <div className="py-5 text-center">
+            <a
+              href="#"
+              className="block text-2xl font-bold text-gray-800 dark:text-white"
+            >
+              John Doe
+            </a>
+            <span className="text-sm text-gray-700 dark:text-gray-200">
+              Software Engineer
+            </span>
+          </div>
+        </div>
+        <div className="max-w-xs mx-2 my-2 overflow-hidden bg-white rounded-lg shadow-lg dark:bg-blue-800">
+          <img
+            className="object-cover w-full h-56"
+            src="https://images.unsplash.com/photo-1542156822-6924d1a71ace?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+            alt="avatar"
+          />
+          <div className="py-5 text-center">
+            <a
+              href="#"
+              className="block text-2xl font-bold text-gray-800 dark:text-white"
+            >
+              John Doe
+            </a>
+            <span className="text-sm text-gray-700 dark:text-gray-200">
+              Software Engineer
+            </span>
+          </div>
+        </div>
+        <div className="max-w-xs mx-2 my-2 overflow-hidden bg-white rounded-lg shadow-lg dark:bg-blue-800">
+          <img
+            className="object-cover w-full h-56"
+            src="https://images.unsplash.com/photo-1542156822-6924d1a71ace?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+            alt="avatar"
+          />
+          <div className="py-5 text-center">
+            <a
+              href="#"
+              className="block text-2xl font-bold text-gray-800 dark:text-white"
+            >
+              John Doe
+            </a>
+            <span className="text-sm text-gray-700 dark:text-gray-200">
+              Software Engineer
+            </span>
+          </div>
+        </div>
+        <div className="max-w-xs mx-2 my-2 overflow-hidden bg-white rounded-lg shadow-lg dark:bg-blue-800">
+          <img
+            className="object-cover w-full h-56"
+            src="https://images.unsplash.com/photo-1542156822-6924d1a71ace?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+            alt="avatar"
+          />
+          <div className="py-5 text-center">
+            <a
+              href="#"
+              className="block text-2xl font-bold text-gray-800 dark:text-white"
+            >
+              John Doe
+            </a>
+            <span className="text-sm text-gray-700 dark:text-gray-200">
+              Software Engineer
+            </span>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  );
 }
-export default miscellaneous
+export default miscellaneous;
