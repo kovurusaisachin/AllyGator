c49e8f91e9555806300347cd8bd42e81396fe04d
Maitreyi S <maitreyis@Maitreyis-MacBook-Pro-2.local>
2022-02-22 21:21:11 -0500

    GET ALL COURSE INFO API Development (sprint 2) #54

--- Backend/main.go
+++ Backend/main.go
@@ -36,8 +36,8 @@ func main() {
 		// v1.PUT("chat/:id", api.UpdateChat)
 
 		//Course APIs
-		v1.GET("course", api.getCourses)
-		v1.GET("course/:id", api.getCourseById)
+		v1.GET("course", api.GetCourses)
+		v1.GET("course/:id", api.GetCourseById)
 	}
 
 	// By default it serves on :8080 unless a
