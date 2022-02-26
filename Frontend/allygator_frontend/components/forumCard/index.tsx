8320d792d67c91491dc50ade6bd93a82e9220895
prashantkapri7@gmail.com <prashantkapri7@gmail.com>
2022-02-26 13:37:26 -0500

    base cypress setup done

--- Frontend/allygator_frontend/components/forumCard/index.tsx
+++ Frontend/allygator_frontend/components/forumCard/index.tsx
@@ -2,138 +2,17 @@ import React from "react";
 export default function ForumCard() {
   return (
     <>
-      <div className="flex flex-col bg-gray-100 ">
-
-        <div className="bg-white shadow">
-          <div className="px-4 sm:px-6 lg:max-w-7xl lg:mx-auto lg:px-8">
-            <div className="py-6 md:flex md:items-center md:justify-between">
-              <div className="flex-1 min-w-0">
-                {/* Profile */}
-                <div className="flex items-center">
-                  <div>
-                    <div className="ml-3 flex flex-col items-start">
-                      <h1 className="text-2xl font-bold leading-7 text-gray-900 sm:leading-9 sm:truncate">
-                        Forum Board
-                      </h1>
-                      <p className="text-gray-600">
-                        Get an overview of all Forum activity here
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <section className="text-gray-600 mx-10 my-10 bg-white rounded-md body-font overflow-hidden">
-          <div className="container px-5 py-24 mx-auto">
-            <div className="-my-8 divide-y-2 divide-gray-100">
-              <div className="py-8 flex flex-wrap md:flex-nowrap">
-                <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                  <span className="font-semibold title-font text-gray-700">
-                    CATEGORY
-                  </span>
-                  <span className="mt-1 text-gray-500 text-sm">12 Jan 2022</span>
-                </div>
-                <div className="md:flex-grow">
-                  <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">
-                    UF offering 5 courses in AI & Machine learning domain for spring 22
-                  </h2>
-                  <p className="leading-relaxed">
-                    Glossier echo park pug, church-key sartorial biodiesel
-                    vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf
-                    moon party messenger bag selfies, poke vaporware kombucha
-                    lumbersexual pork belly polaroid hoodie portland craft beer.
-                  </p>
-                  <a className="text-green-500 inline-flex items-center mt-4">
-                    Learn More
-                    <svg
-                      className="w-4 h-4 ml-2"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                      fill="none"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    >
-                      <path d="M5 12h14" />
-                      <path d="M12 5l7 7-7 7" />
-                    </svg>
-                  </a>
-                </div>
-              </div>
-              <div className="py-8 flex flex-wrap md:flex-nowrap">
-                <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                  <span className="font-semibold title-font text-gray-700">
-                    CATEGORY
-                  </span>
-                  <span className="mt-1 text-gray-500 text-sm">07 Nov 2020</span>
-                </div>
-                <div className="md:flex-grow">
-                  <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">
-                    Four student from UF got 1 million dollar offer from google, facebook and microsoft
-                  </h2>
-                  <p className="leading-relaxed">
-                    Glossier echo park pug, church-key sartorial biodiesel
-                    vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf
-                    moon party messenger bag selfies, poke vaporware kombucha
-                    lumbersexual pork belly polaroid hoodie portland craft beer.
-                  </p>
-                  <a className="text-green-500 inline-flex items-center mt-4">
-                    Learn More
-                    <svg
-                      className="w-4 h-4 ml-2"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                      fill="none"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    >
-                      <path d="M5 12h14" />
-                      <path d="M12 5l7 7-7 7" />
-                    </svg>
-                  </a>
-                </div>
-              </div>
-              <div className="py-8 flex flex-wrap md:flex-nowrap">
-                <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                  <span className="font-semibold title-font text-gray-700">
-                    CATEGORY
-                  </span>
-                  <span className="text-sm text-gray-500">18 Aug 2020</span>
-                </div>
-                <div className="md:flex-grow">
-                  <h2 className="text-2xl font-medium text-gray-900 title-font mb-2">
-                    UF ranked in top 5 public university over nation
-                  </h2>
-                  <p className="leading-relaxed">
-                    Glossier echo park pug, church-key sartorial biodiesel
-                    vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf
-                    moon party messenger bag selfies, poke vaporware kombucha
-                    lumbersexual pork belly polaroid hoodie portland craft beer.
-                  </p>
-                  <a className="text-green-500 inline-flex items-center mt-4">
-                    Learn More
-                    <svg
-                      className="w-4 h-4 ml-2"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                      fill="none"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                    >
-                      <path d="M5 12h14" />
-                      <path d="M12 5l7 7-7 7" />
-                    </svg>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-      </div>
+    <ul className="mx-10 my-5 p-4 rounded-lg lg:p-8 bg-white dark:bg-coolGray-800 dark:text-coolGray-100">
+	<li>
+		<article>
+			<a rel="noopener noreferrer" href="#" className="grid p-4 overflow-hidden md:grid-cols-5 rounded-xl lg:p-6 xl:grid-cols-12 hover:dark:bg-coolGray-900">
+				<h3 className="mb-1 ml-8 font-semibold md:col-start-2 md:col-span-4 md:ml-0 xl:col-start-3 xl:col-span-9">Earum at ipsa aliquid quis, exercitationem est.</h3>
+				<time  className="row-start-1 mb-1 md:col-start-1 xl:col-span-2 dark:text-coolGray-400">Oct 13, 2020</time>
+				<p className="ml-8 md:col-start-2 md:col-span-4 xl:col-start-3 xl:col-span-9 md:ml-0 dark:text-coolGray-300">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique saepe exercitationem numquam, labore necessitatibus deleniti quasi. Illo porro nihil necessitatibus debitis delectus aperiam, fuga impedit assumenda odit, velit eveniet est.</p>
+			</a>
+		</article>
+	</li>
+</ul>
     </>
   );
 }
--- Frontend/allygator_frontend/pages/dashboard/forum/index.tsx
+++ Frontend/allygator_frontend/pages/dashboard/forum/index.tsx
@@ -3,10 +3,110 @@ import Sidebar from '../../../components/sidebar'
 import ForumCard from '../../../components/forumCard'
 // import ResetPass from './ResetPassword'
 function Forum() {
-    return(
+    return (
         <div className="flex flex-col-2">
             <Sidebar />
-            <ForumCard />
+            <div className="flex flex-col bg-gray-100 ">
+
+                <div className="bg-white shadow">
+                    <div className="px-4 sm:px-6 lg:max-w-7xl lg:mx-auto lg:px-8">
+                        <div className="py-6 md:flex md:items-center md:justify-between">
+                            <div className="flex-1 min-w-0">
+                                {/* Profile */}
+                                <div className="flex items-center">
+                                    <div>
+                                        <div className="ml-3 flex flex-col items-start">
+                                            <h1 className="text-2xl font-bold leading-7 text-gray-900 sm:leading-9 sm:truncate">
+                                                Forum Board
+                                            </h1>
+                                            <p className="text-gray-600">
+                                                Get an overview of all Forum activity here
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="tags" className="flex flex-col-1 ml-10 my-5 mr-10">
+
+                    <button className="flex items-center px-3 py-1 font-small tracking-wide text-white capitalize transition-colors duration-200 transform bg-gray-400 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                        <span className="mx-1">New</span>
+                    </button>
+                    <span className="mx-4"></span>
+                    <button className="flex items-center px-3 py-1 font-small tracking-wide text-white capitalize transition-colors duration-200 transform bg-gray-400 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />
+                        </svg>
+                        <span className="mx-1">Top</span>
+                    </button>
+                    <span className="mx-4"></span>
+                    <button className="flex items-center px-3 py-1 font-small tracking-wide text-white capitalize transition-colors duration-200 transform bg-gray-400 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17.657 18.657A8 8 0 016.343 7.343S7 9 9 10c0-2 .5-5 2.986-7C14 5 16.09 5.777 17.656 7.343A7.975 7.975 0 0120 13a7.975 7.975 0 01-2.343 5.657z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.879 16.121A3 3 0 1012.015 11L11 14H9c0 .768.293 1.536.879 2.121z" />
+                        </svg>
+                        <span className="mx-1">Hot</span>
+                    </button>
+                    
+                </div>
+                <div className=''>
+                <span className='flex flex-col-2 float-right'>
+                    <button className="flex items-center px-3 py-1 font-small tracking-wide text-white capitalize transition-colors duration-200 transform bg-gray-400 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+</svg>
+                        <span className="mx-1">Add</span>
+                    </button>
+                    </span>
+                </div>
+
+                <section className="text-gray-600 mx-10 bg-white rounded-md body-font overflow-hidden">
+                    <div className="col-span-2">
+                        <div className="w-full">
+                            <div className="relative">
+                                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                                    <svg
+                                        className="h-5 w-5 text-gray-400"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 20 20"
+                                        fill="currentColor"
+                                        aria-hidden="true"
+                                    >
+                                        <path
+                                            fillRule="evenodd"
+                                            d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                            clipRule="evenodd"
+                                        />
+                                    </svg>
+                                </div>
+                                <input
+                                    className="block md:text-sm w-full pl-10 pr-3 py-2 border-2 border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-primary-500 focus:border-primary-500 sm:text-lg"
+                                    placeholder="Search"
+                                    type="text"
+                                // value={state.query.searchText}
+                                // onChange={e => {
+                                //   setState({
+                                //     ...state,
+                                //     query: {
+                                //       ...state.query,
+                                //       searchText: e.target.value
+                                //     }
+                                //   });
+                                // }}
+                                />
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <ForumCard />
+                
+            </div>
+            
         </div>
     )
 }
