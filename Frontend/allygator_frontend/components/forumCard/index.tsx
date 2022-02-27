5b28de24d0a6f1e184684a1522f3e21bee1c5d5f
prashantkapri7@gmail.com <prashantkapri7@gmail.com>
2022-02-26 19:17:59 -0500

    forum new screen design done

--- Frontend/allygator_frontend/components/forumCard/index.tsx
+++ Frontend/allygator_frontend/components/forumCard/index.tsx
@@ -1,18 +1,64 @@
 import React from "react";
-export default function ForumCard() {
+export default function ForumCard(props) {
+	console.log(props,'pnnpnpn')
   return (
     <>
-    <ul className="mx-10 my-5 p-4 rounded-lg lg:p-8 bg-white dark:bg-coolGray-800 dark:text-coolGray-100">
-	<li>
-		<article>
-			<a rel="noopener noreferrer" href="#" className="grid p-4 overflow-hidden md:grid-cols-5 rounded-xl lg:p-6 xl:grid-cols-12 hover:dark:bg-coolGray-900">
-				<h3 className="mb-1 ml-8 font-semibold md:col-start-2 md:col-span-4 md:ml-0 xl:col-start-3 xl:col-span-9">Earum at ipsa aliquid quis, exercitationem est.</h3>
-				<time  className="row-start-1 mb-1 md:col-start-1 xl:col-span-2 dark:text-coolGray-400">Oct 13, 2020</time>
-				<p className="ml-8 md:col-start-2 md:col-span-4 xl:col-start-3 xl:col-span-9 md:ml-0 dark:text-coolGray-300">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique saepe exercitationem numquam, labore necessitatibus deleniti quasi. Illo porro nihil necessitatibus debitis delectus aperiam, fuga impedit assumenda odit, velit eveniet est.</p>
-			</a>
-		</article>
-	</li>
-</ul>
+      <div className="mx-10 my-5 rounded-lg bg-white dark:bg-coolGray-800 dark:text-coolGray-100">
+        <div className="container max-w-4xl px-10 py-6 mx-auto rounded-lg shadow-sm dark:bg-coolGray-900">
+          <div>
+            <a rel="noopener noreferrer" href="#" className="flex items-center">
+              <span className="font-bold hover:underline dark:text-coolGray-400">
+                {props.data?.name ?? ""}
+              </span>
+            </a>
+          </div>
+          <div className="flex items-center justify-between">
+            <span className="text-sm dark:text-coolGray-400">{props.data?.time ?? ""}</span>
+            {/* <a
+              rel="noopener noreferrer"
+              href="#"
+              className="px-2 py-1 font-bold rounded dark:bg-violet-400 dark:text-coolGray-900"
+            >
+              Javascript
+            </a> */}
+          </div>
+          <div className="mt-3">
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              className="text-xl font-bold hover:underline"
+            >
+              {props?.data?.title ?? ""}
+            </a>
+            <p className="mt-2">
+             {props?.data?.content ?? ""}
+            </p>
+          </div>
+          <div className="flex flex-col-2">
+
+            <div className="flex items-center gap-4 justify-start mt-4">
+			{	
+				props?.data?.tags?.map( (x) => {
+					<button className="flex items-center px-3 py-1 font-small tracking-wide text-white capitalize transition-colors duration-200 transform bg-gray-400 rounded-full hover:bg-gray-700 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
+                <span className="mx-1">{x}</span>
+              </button>
+				})
+			}
+            </div>
+            <div className="flex items-end gap-4 justify-end mt-4">
+              <button className="flex items-center px-3 py-1 font-small tracking-wide text-white capitalize transition-colors duration-200 transform bg-gray-400 rounded-full hover:bg-gray-700 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
+                <span className="mx-1">ux Design</span>
+              </button>
+              <button className="flex items-center px-3 py-1 font-small tracking-wide text-white capitalize transition-colors duration-200 transform bg-gray-400 rounded-full hover:bg-gray-700 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
+                <span className="mx-1">HCI</span>
+              </button>
+              <button className="flex items-center px-3 py-1 font-small tracking-wide text-white capitalize transition-colors duration-200 transform bg-gray-400 rounded-full hover:bg-gray-700 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80">
+                <span className="mx-1">UI</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
