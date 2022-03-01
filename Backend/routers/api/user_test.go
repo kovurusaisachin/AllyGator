4b48da0b54aa2eabb268e39c2bc2fe04c3bfe274
Sai Sachin Kovuru <saisachinkovuru@Sais-MacBook-Pro.local>
2022-03-01 14:56:25 -0500

    GO-Test cases for ADD-USER-API #76

---
+++ Backend/routers/api/user_test.go
@@ -0,0 +1,132 @@
+// Golang REST API unit testing program
+package api
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"allygator.com/gatorweb/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+var existingEmail = []byte(`{
+	"firstname": "SaiSachin",
+	"lastname": "Kovuru",
+	"department": 111,
+	"password": "",
+	"email": "kovuru.saisachin@ufl.edu",
+	"gender": "male",
+	"course": "Software engineering",
+	"url": "https://www.linkedin.com/in/saisachinkovuru/",
+	"nationality": "Indian",
+	"profile": "Cloud",
+	"specialization": "Backend",
+	"status": "active"
+	}`)
+
+var uniqueEmail = []byte(`{
+	"firstname": "SaiSachin",
+	"lastname": "Kovuru",
+	"department": 111,
+	"password": "",
+	"email": "unique@ufl.edu",
+	"gender": "male",
+	"course": "Software engineering",
+	"url": "https://www.linkedin.com/in/saisachinkovuru/",
+	"nationality": "Indian",
+	"profile": "Cloud",
+	"specialization": "Backend",
+	"status": "active"
+	}`)
+
+var emptyData = []byte(``)
+
+//AddUser API mock test
+func TestAddUser(t *testing.T) {
+	err := models.ConnectDatabase()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Switching to test mode so we don't get such noisy output
+	gin.SetMode(gin.TestMode)
+
+	// Settingup the router, and
+	// registering the routes
+	r := gin.Default()
+	v1 := r.Group("/api/v1")
+	v1.POST("register", AddUser)
+
+	t.Run("Empty JSON Data", func(t *testing.T) {
+
+		req, err := http.NewRequest(http.MethodPost, "/api/v1/register", bytes.NewBuffer(emptyData))
+		if err != nil {
+			t.Fatalf("Couldn't create request: %v\n", err)
+		}
+
+		// Creating a response recorder so that we can inspect the response
+		w := httptest.NewRecorder()
+
+		// Performing the request
+		fmt.Print("\nMock API for Add User - Sending Empty Data\n")
+		r.ServeHTTP(w, req)
+		fmt.Println(w.Body)
+		fmt.Print("\n\n")
+		// Checking if the response was what we expected
+		if w.Code == http.StatusOK {
+			t.Logf("Expected to get status %d is same ast %d\n", http.StatusOK, w.Code)
+		} else {
+			t.Logf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
+		}
+	})
+
+	t.Run("Email ID Already exists", func(t *testing.T) {
+
+		req, err := http.NewRequest(http.MethodPost, "/api/v1/register", bytes.NewBuffer(existingEmail))
+		if err != nil {
+			t.Fatalf("Couldn't create request: %v\n", err)
+		}
+
+		// Creating a response recorder so that we can inspect the response
+		w := httptest.NewRecorder()
+
+		// Performing the request
+		fmt.Print("\nMock API for Add User - Sending the existing EmailID\n")
+		r.ServeHTTP(w, req)
+		fmt.Println(w.Body)
+		fmt.Print("\n\n")
+		// Checking if the response was what we expected
+		if w.Code == http.StatusOK {
+			t.Logf("Expected to get status %d is same ast %d\n", http.StatusOK, w.Code)
+		} else {
+			t.Logf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
+		}
+	})
+
+	t.Run("Unique Email ID", func(t *testing.T) {
+
+		req, err := http.NewRequest(http.MethodPost, "/api/v1/register", bytes.NewBuffer(uniqueEmail))
+		if err != nil {
+			t.Fatalf("Couldn't create request: %v\n", err)
+		}
+
+		// Creating a response recorder so that we can inspect the response
+		w := httptest.NewRecorder()
+
+		// Performing the request
+		fmt.Print("\nMock API for Add User - Sending the unique EmailID\n")
+		r.ServeHTTP(w, req)
+		fmt.Println(w.Body)
+		fmt.Print("\n\n")
+		// Checking if the response was what we expected
+		if w.Code == http.StatusOK {
+			t.Logf("Expected to get status %d is same ast %d\n", http.StatusOK, w.Code)
+		} else {
+			t.Logf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
+		}
+	})
+}
