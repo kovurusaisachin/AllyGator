4d90237e311d32da10a74d41bf01951427d2b1bb
Sai Sachin Kovuru <saisachinkovuru@Sais-MacBook-Pro.local>
2022-03-01 16:14:53 -0500

    GO-Test cases for DELETE-USER-BY-ID-API #76

--- Backend/routers/api/user_test.go
+++ Backend/routers/api/user_test.go
@@ -360,3 +360,67 @@ func TestUpdateUser(t *testing.T) {
 		}
 	})
 }
+
+//DELETE User API mock test
+func TestDeleteUser(t *testing.T) {
+	err := models.ConnectDatabase()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Switching to test mode so we don't get such noisy output
+	gin.SetMode(gin.TestMode)
+
+	// Settingup the router, and
+	// registering the routes
+	r := gin.Default()
+	v1 := r.Group("/api/v1")
+	v1.DELETE("user/:id", DeleteUser)
+
+	t.Run("Student ID does not exist", func(t *testing.T) {
+
+		req, err := http.NewRequest(http.MethodDelete, "/api/v1/user/77", nil)
+		if err != nil {
+			t.Fatalf("Couldn't create request: %v\n", err)
+		}
+
+		// Creating a response recorder so that we can inspect the response
+		w := httptest.NewRecorder()
+
+		// Performing the request
+		fmt.Print("\nMock API for Deleting the User - Sending the wrong StudentID\n")
+		r.ServeHTTP(w, req)
+		fmt.Print("\n\n")
+		fmt.Println(w.Body)
+		fmt.Print("\n\n")
+		// Checking if the response was what we expected
+		if w.Code == http.StatusOK {
+			t.Logf("Expected to get status %d is same ast %d\n", http.StatusOK, w.Code)
+		} else {
+			t.Logf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
+		}
+	})
+
+	t.Run("Student ID exists", func(t *testing.T) {
+
+		req, err := http.NewRequest(http.MethodDelete, "/api/v1/user/7", nil)
+		if err != nil {
+			t.Fatalf("Couldn't create request: %v\n", err)
+		}
+
+		// Creating a response recorder so that we can inspect the response
+		w := httptest.NewRecorder()
+
+		// Performing the request
+		fmt.Print("\nMock API for Deleting the User - Sending the valid StudentID\n")
+		r.ServeHTTP(w, req)
+		fmt.Print("\n\n")
+		fmt.Println(w.Body)
+		fmt.Print("\n\n")
+		// Checking if the response was what we expected
+		if w.Code == http.StatusOK {
+			t.Logf("Expected to get status %d is same ast %d\n", http.StatusOK, w.Code)
+		} else {
+			t.Logf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
+		}
+	})
+}
