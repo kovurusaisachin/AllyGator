af9a7dd04b777e04b0a00e4fbfbc706e3f62f803
Sai Sachin Kovuru <saisachinkovuru@Sais-MacBook-Pro.local>
2022-03-02 19:52:18 -0500

    GO-Test cases for ADD-FACULTY-API #76

---
+++ Backend/routers/api/faculty_test.go
@@ -0,0 +1,118 @@
+// Golang REST API unit testing program
+package api
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"allygator.com/gatorweb/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+var existingFaculty = []byte(`{
+	"idFaculty": 20,
+	"facultyname": "AlinDobra",
+	"idDepartment": 111,
+	"info": "Has 20 years of experience in the Computer Science research domain",
+	"deptName": "CSE"
+}`)
+
+var uniqueFaculty = []byte(`{
+    "idFaculty": 26,
+    "facultyname": "Sai Srujan Kovuru",
+    "idDepartment": 111,
+    "info": "Has 7 years of experience in the Computer Science research domain",
+    "deptName": "CSE"		
+}`)
+
+var emptyData = []byte(``) // Remove this string afer merging into main branch
+
+//AddFaculty API mock test
+func TestAddFaculty(t *testing.T) {
+	err := models.ConnectDatabase()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Switching to test mode so we don't get such noisy output
+	gin.SetMode(gin.TestMode)
+
+	// Settingup the router, and
+	// registering the routes
+	r := gin.Default()
+	v1 := r.Group("/api/v1")
+	v1.POST("addFaculty", AddFaculty)
+
+	t.Run("Empty JSON Data", func(t *testing.T) {
+
+		req, err := http.NewRequest(http.MethodPost, "/api/v1/addFaculty", bytes.NewBuffer(emptyData))
+		if err != nil {
+			t.Fatalf("Couldn't create request: %v\n", err)
+		}
+
+		// Creating a response recorder so that we can inspect the response
+		w := httptest.NewRecorder()
+
+		// Performing the request
+		fmt.Print("\nMock API for Add Faculty- Sending Empty Data\n")
+		r.ServeHTTP(w, req)
+		fmt.Println(w.Body)
+		fmt.Print("\n\n")
+		// Checking if the response was what we expected
+		if w.Code == http.StatusOK {
+			t.Logf("Expected to get status %d is same ast %d\n", http.StatusOK, w.Code)
+		} else {
+			t.Logf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
+		}
+	})
+
+	t.Run("Faculty ID Already exists", func(t *testing.T) {
+
+		req, err := http.NewRequest(http.MethodPost, "/api/v1/addFaculty", bytes.NewBuffer(existingFaculty))
+		if err != nil {
+			t.Fatalf("Couldn't create request: %v\n", err)
+		}
+
+		// Creating a response recorder so that we can inspect the response
+		w := httptest.NewRecorder()
+
+		// Performing the request
+		fmt.Print("\nMock API for Add Faculty - Sending the existing FacultyID\n")
+		r.ServeHTTP(w, req)
+		fmt.Println(w.Body)
+		fmt.Print("\n\n")
+		// Checking if the response was what we expected
+		if w.Code == http.StatusOK {
+			t.Logf("Expected to get status %d is same ast %d\n", http.StatusOK, w.Code)
+		} else {
+			t.Logf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
+		}
+	})
+
+	t.Run("Unique Faculty ID", func(t *testing.T) {
+
+		req, err := http.NewRequest(http.MethodPost, "/api/v1/addFaculty", bytes.NewBuffer(uniqueFaculty))
+		if err != nil {
+			t.Fatalf("Couldn't create request: %v\n", err)
+		}
+
+		// Creating a response recorder so that we can inspect the response
+		w := httptest.NewRecorder()
+
+		// Performing the request
+		fmt.Print("\nMock API for Add Faculty - Sending the unique FacultyID\n")
+		r.ServeHTTP(w, req)
+		fmt.Println(w.Body)
+		fmt.Print("\n\n")
+		// Checking if the response was what we expected
+		if w.Code == http.StatusOK {
+			t.Logf("Expected to get status %d is same ast %d\n", http.StatusOK, w.Code)
+		} else {
+			t.Logf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
+		}
+	})
+}
