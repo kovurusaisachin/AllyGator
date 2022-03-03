ef87a8b36632a0908a06311e6dd8a8a8d9206690
Noopur Thanvi <noopurthanvi@Noopurs-MacBook-Air.local>
2022-03-02 23:48:15 -0500

    misc page header updated

--- Frontend/allygator_frontend/pages/dashboard/miscellaneous/ethnicfoodstores/index.tsx
+++ Frontend/allygator_frontend/pages/dashboard/miscellaneous/ethnicfoodstores/index.tsx
@@ -2,10 +2,30 @@ import Sidebar from "../../../../components/sidebar"
 export default function ethnicfoodstores () {
     return (
         <div className="flex flex-col-2">
-        <Sidebar />
-        <h2>More Content Coming Soon!!!</h2>
-        
-        
-    </div>
+      <Sidebar />
+      <div className="flex flex-col bg-gray-200 w-full">
+        <div className="bg-white shadow">
+          <div className="px-4 sm:px-6 lg:max-w-7xl lg:mx-auto lg:px-8">
+            <div className="py-6 md:flex md:items-center md:justify-between">
+              <div className="flex-1 min-w-0">
+                <div className="flex items-center">
+                  <div>
+                    <div className="ml-3 flex flex-col items-start">
+                      <h3 className="text-2xl font-bold leading-7 text-gray-900 sm:leading-9 sm:truncate">
+                        Food Stores Details
+                      </h3>
+                      <p className="text-gray-600">
+                        Get an overview of all Food Stores related details here
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-10 bg-blue-100"></div>
+      </div>
+      </div>
     )
 }
