4e85004c1706730ed16db53c46b932b97ad0b33b
Maitreyi S <maitreyis@Maitreyis-MacBook-Pro-2.local>
2022-03-04 01:50:24 -0500

    Ethnic food stores page 1 added

--- Frontend/allygator_frontend/pages/dashboard/miscellaneous/ethnicfoodstores/index.tsx
+++ Frontend/allygator_frontend/pages/dashboard/miscellaneous/ethnicfoodstores/index.tsx
@@ -3,8 +3,131 @@ export default function ethnicfoodstores () {
     return (
         <div className="flex flex-col-2">
         <Sidebar />
-        <h2>More Content Coming Soon!!!</h2>
-        
+        <section className="w-full px-4 py-24 mx-auto max-w-7xl md:w-3/4 lg:w-2/4">
+  <div className="mb-12 text-left md:text-center">
+    <h2 className="mb-2 text-3xl font-extrabold leading-tight text-gray-900">Ethnic Food Stores</h2>
+    <p className="text-lg text-gray-500">Collection of ethnic food stores in Gainesville.</p>
+  </div>
+  <div className="flex flex-col space-y-12 divide-y divide-gray-200">
+    <div>
+      {/* <p className="pt-12 mb-3 text-sm font-normal text-gray-500">April 16, 2020</p> */}
+      <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
+        <a href="#" className="text-gray-900 hover:text-purple-700">Asian Food Market/Oriental Food & Gift Market</a>
+      </h2>
+      <p className="mb-4 text-base font-normal text-gray-600">
+       3345 SW 34th St.
+       Gainesville FL 32608
+       352-372-4591
+      </p>
+    </div>
+    <div>
+      {/* <p className="pt-12 mb-3 text-sm font-normal text-gray-500">April 16, 2020</p> */}
+      <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
+        <a href="#" className="text-gray-900 hover:text-purple-700">Eastern Market</a>
+      </h2>
+      <p className="mb-4 text-base font-normal text-gray-600">
+        1349 NW 23rd Ave.
+        Gainesville FL 32605
+        352-371-3085
+      </p>
+      {/* <a href="#" className="btn btn-light btn-sm">Continue Reading</a> */}
+    </div>
+    <div>
+      {/* <p className="pt-12 mb-3 text-sm font-normal text-gray-500">April 16, 2020</p> */}
+      <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
+        <a href="#" className="text-gray-900 hover:text-purple-700">Chun Ching Market</a>
+      </h2>
+      <p className="mb-4 text-base font-normal text-gray-600">
+        418 NW 8th Ave
+        Gainesville FL 32601
+        352-376-5885
+      </p>
+      {/* <a href="#" className="btn btn-light btn-sm">Continue Reading</a> */}
+    </div>
+    <div>
+      {/* <p className="pt-12 mb-3 text-sm font-normal text-gray-500">April 16, 2020</p> */}
+      <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
+        <a href="#" className="text-gray-900 hover:text-purple-700">Falafel King Sandwiches</a>
+      </h2>
+      <p className="mb-4 text-base font-normal text-gray-600">
+        3252 SW 35th Blvd.
+        Gainesville FL 32605
+        352-375-6342
+      </p>
+      {/* <a href="#" className="btn btn-light btn-sm">Continue Reading</a> */}
+    </div>
+    <div>
+      {/* <p className="pt-12 mb-3 text-sm font-normal text-gray-500">April 16, 2020</p> */}
+      <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
+      <a href="#" className="text-gray-900 hover:text-purple-700">Gainesville Russian Food</a>
+      </h2>
+      <p className="mb-4 text-base font-normal text-gray-600">
+       5200 NW 34th Blvd.
+        Gainesville  FL 32605
+        352-727-7707
+      </p> 
+    </div>
+    <div>
+      {/* <p className="pt-12 mb-3 text-sm font-normal text-gray-500">April 16, 2020</p> */}
+      <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
+      <a href="#" className="text-gray-900 hover:text-purple-700">India Bazaar</a>
+      </h2>
+      <p className="mb-4 text-base font-normal text-gray-600">
+       3550 SW 34th St.
+        Gainesville  FL 32608
+        352-271-7571
+      </p> 
+    </div>
+    <div>
+      {/* <p className="pt-12 mb-3 text-sm font-normal text-gray-500">April 16, 2020</p> */}
+      <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
+      <a href="#" className="text-gray-900 hover:text-purple-700">India Spice</a>
+      </h2>
+      <p className="mb-4 text-base font-normal text-gray-600">
+       4110 SW 34th St. #4
+        Gainesville  FL 32608
+        352-692-4969
+      </p> 
+    </div>
+    <div>
+      {/* <p className="pt-12 mb-3 text-sm font-normal text-gray-500">April 16, 2020</p> */}
+      <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
+      <a href="#" className="text-gray-900 hover:text-purple-700">La Aurora Latin Market</a>
+      </h2>
+      <p className="mb-4 text-base font-normal text-gray-600">
+       3773 W University Ave.
+        Gainesville  FL 32607
+        352-271-9035
+      </p> 
+    </div>
+    <div>
+      {/* <p className="pt-12 mb-3 text-sm font-normal text-gray-500">April 16, 2020</p> */}
+      <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
+      <a href="#" className="text-gray-900 hover:text-purple-700">Philippine Express</a>
+      </h2>
+      <p className="mb-4 text-base font-normal text-gray-600">
+       4000 W Newberry Rd. Suite A
+        Gainesville  FL 32607
+        352-375-0080
+      </p> 
+    </div>
+    <div>
+      {/* <p className="pt-12 mb-3 text-sm font-normal text-gray-500">April 16, 2020</p> */}
+      <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
+      <a href="#" className="text-gray-900 hover:text-purple-700">Zeezeenia Kitchen and Market</a>
+      </h2>
+      <p className="mb-4 text-base font-normal text-gray-600">
+       2325 SW 13th St.
+        Gainesville  FL 32608
+        352-505-3856
+      </p> 
+    </div>
+      </div>
+  {/* <div className="flex flex-col items-center justify-center pt-12 mt-12 space-x-0 space-y-2 border-t border-gray-200 md:space-x-2 md:space-y-0 md:flex-row">
+    <a href="#" className="w-full rounded-full btn btn-light btn-xl md:w-auto">Previous Page</a>
+    <a href="#" className="w-full rounded-full btn btn-light btn-xl md:w-auto">Next Page</a>
+  </div> */}
+</section>
         
     </div>
     )
