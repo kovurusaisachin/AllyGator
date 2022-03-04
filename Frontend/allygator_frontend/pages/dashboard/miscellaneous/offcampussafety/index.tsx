d364d1610ade5c3cb7467f0d121ff6677162c53e
Maitreyi S <maitreyis@Maitreyis-MacBook-Pro-2.local>
2022-03-04 02:01:12 -0500

    Off campus safety - page added

--- Frontend/allygator_frontend/pages/dashboard/miscellaneous/offcampussafety/index.tsx
+++ Frontend/allygator_frontend/pages/dashboard/miscellaneous/offcampussafety/index.tsx
@@ -3,9 +3,11 @@ export default function offcampussafety () {
     return (
         <div className="flex flex-col-2">
         <Sidebar />
-        <h2>More Content Coming Soon!!!</h2>
-        
-        
-    </div>
+        <section className="w-full px-4 py-24 mx-auto max-w-7xl md:w-3/4 lg:w-2/4">
+  <div className="mb-12 text-left md:text-center">
+    <h2 className="mb-2 text-3xl font-extrabold leading-tight text-gray-900">Off campus safety</h2>
+    <p className="text-lg text-gray-500">Off campus safety from the dean of students office.Gainesville is considered a safe community, but it is not immune from criminal activity. However, common sense precautions can reduce your vulnerability to both personal and property crimes.</p>
+  </div>
+  
     )
 }
