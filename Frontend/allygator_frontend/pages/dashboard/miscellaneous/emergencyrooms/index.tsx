e4151605fa34ceed6ab2f2e5d4ae8f7c56192fdc
Maitreyi S <maitreyis@Maitreyis-MacBook-Pro-2.local>
2022-03-04 01:32:16 -0500

    Emergency rooms page - Part 2

--- Frontend/allygator_frontend/pages/dashboard/miscellaneous/emergencyrooms/index.tsx
+++ Frontend/allygator_frontend/pages/dashboard/miscellaneous/emergencyrooms/index.tsx
@@ -81,6 +81,66 @@ export default function emergencyrooms () {
     </p> 
     </h2> 
   </div>
+  <div>
+    {/* <p className="pt-12 mb-3 text-sm font-normal text-gray-500">April 16, 2020</p> */}
+    <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
+    <a href="#" className="text-gray-900 hover:text-purple-700">UF Health Primary Care After Hours</a>
+    <p className="mb-4 text-base font-normal text-gray-600">
+      3450 Hull Rd. 2nd Floor
+      Gainesville FL 32607
+      352-265-1234
+     Appointment only
+    </p> 
+    </h2> 
+  </div>
+  <div>
+    {/* <p className="pt-12 mb-3 text-sm font-normal text-gray-500">April 16, 2020</p> */}
+    <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
+    <a href="#" className="text-gray-900 hover:text-purple-700">North Florida Regional Medical Center Emergency Room</a>
+    <p className="mb-4 text-base font-normal text-gray-600">
+     6500 W Newberry Rd.
+      Gainesville FL 32605
+      352-415-4439
+     Open 24/7
+    </p> 
+    </h2> 
+  </div>
+  <div>
+    {/* <p className="pt-12 mb-3 text-sm font-normal text-gray-500">April 16, 2020</p> */}
+    <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
+    <a href="#" className="text-gray-900 hover:text-purple-700">North Florida Regional Medical Center Express Emergency Room</a>
+    <p className="mb-4 text-base font-normal text-gray-600">
+      812 NW 57th St.
+      Gainesville FL 32605
+      352-519-5420
+     8am - 4:30 pm
+    </p> 
+    </h2> 
+  </div>
+  <div>
+    {/* <p className="pt-12 mb-3 text-sm font-normal text-gray-500">April 16, 2020</p> */}
+    <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
+    <a href="#" className="text-gray-900 hover:text-purple-700">Emergency Physicians Medical Center</a>
+    <p className="mb-4 text-base font-normal text-gray-600">
+      2445 SW 76th St. Suite 110
+      Gainesville FL 32608
+      352-872-5111
+      emergencypmc.com
+    </p> 
+    </h2> 
+  </div>
+  <div>
+    {/* <p className="pt-12 mb-3 text-sm font-normal text-gray-500">April 16, 2020</p> */}
+    <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
+    <a href="#" className="text-gray-900 hover:text-purple-700">Emergency Physicians Medical Center</a>
+    <p className="mb-4 text-base font-normal text-gray-600">
+      9181 NW 39th Ave.
+      Gainesville FL 32606
+      352-727-7755gi
+      emergencypmc.com
+    </p> 
+    </h2> 
+  </div>
     </div>
 {/* <div className="flex flex-col items-center justify-center pt-12 mt-12 space-x-0 space-y-2 border-t border-gray-200 md:space-x-2 md:space-y-0 md:flex-row">
   <a href="#" className="w-full rounded-full btn btn-light btn-xl md:w-auto">Previous Page</a>
