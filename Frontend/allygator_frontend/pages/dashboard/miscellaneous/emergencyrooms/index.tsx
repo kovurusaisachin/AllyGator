e666f039eefd1f8e3ec7fec0603e04ee140c2ef0
Maitreyi S <maitreyis@Maitreyis-MacBook-Pro-2.local>
2022-03-04 01:10:43 -0500

    Emergency rooms page part 1

--- Frontend/allygator_frontend/pages/dashboard/miscellaneous/emergencyrooms/index.tsx
+++ Frontend/allygator_frontend/pages/dashboard/miscellaneous/emergencyrooms/index.tsx
@@ -1,11 +1,93 @@
 import Sidebar from "../../../../components/sidebar"
 export default function emergencyrooms () {
     return (
-        <div className="flex flex-col-2">
-        <Sidebar />
-        <h2>More Content Coming Soon!!!</h2>
-        
-        
+      <div className="flex flex-col-2">
+      <Sidebar />
+      <section className="w-full px-4 py-24 mx-auto max-w-7xl md:w-3/4 lg:w-2/4">
+<div className="mb-12 text-left md:text-center">
+  <h2 className="mb-2 text-3xl font-extrabold leading-tight text-gray-900">Emergency Rooms</h2>
+  <p className="text-lg text-gray-500">List of emergency health care centers.</p>
+</div>
+<div className="flex flex-col space-y-12 divide-y divide-gray-200">
+  <div>
+    {/* <p className="pt-12 mb-3 text-sm font-normal text-gray-500">April 16, 2020</p> */}
+    <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
+      <a href="#" className="text-gray-900 hover:text-purple-700">UF Health Shands Emergency Room/ Trauma Center</a>
+    </h2>
+    <p className="mb-4 text-base font-normal text-gray-600">
+      1515 SW Archer Rd.
+      Gainesville FL 32608
+      352-733-0111
+      Open 24/7
+    </p>
+  </div>
+  <div>
+    {/* <p className="pt-12 mb-3 text-sm font-normal text-gray-500">April 16, 2020</p> */}
+    <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
+      <a href="#" className="text-gray-900 hover:text-purple-700">UF Health Shands Emergency Center - Springhill</a>
+    </h2>
+    <p className="mb-4 text-base font-normal text-gray-600">
+     8475 NW 39th Ave.
+     Gainesville FL 32606
+     352-627-0400
+     Open 24/7
+    </p>
+  </div>
+  <div>
+    {/* <p className="pt-12 mb-3 text-sm font-normal text-gray-500">April 16, 2020</p> */}
+    <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
+      <a href="#" className="text-gray-900 hover:text-purple-700">UF Health Shands Emergency Center - Kanapaha</a>
+    </h2>
+    <p className="mb-4 text-base font-normal text-gray-600">
+      7405 SW Archer Rd.
+      Gainesville FL 32608
+      352-627-0500
+      Open 24/7
+    </p>
+  </div>
+  <div>
+    {/* <p className="pt-12 mb-3 text-sm font-normal text-gray-500">April 16, 2020</p> */}
+    <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
+      <a href="#" className="text-gray-900 hover:text-purple-700">UF Health Pediatrics Emergency Room</a>
+    </h2>
+    <p className="mb-4 text-base font-normal text-gray-600">
+     1600 SW Archer Rd.
+     Gainesville FL 32608
+     352-265-5437
+     Open 24/7
+    </p>
+  </div>
+  <div>
+    {/* <p className="pt-12 mb-3 text-sm font-normal text-gray-500">April 16, 2020</p> */}
+    <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
+    <a href="#" className="text-gray-900 hover:text-purple-700">UF Health Pediatric After Hours</a>
+    <p className="mb-4 text-base font-normal text-gray-600">
+      1600 SW Archer Rd.
+      Gainesville FL 32608
+      352-265-0724
+      Appointment only
+    </p> 
+    </h2> 
+  </div>
+  <div>
+    {/* <p className="pt-12 mb-3 text-sm font-normal text-gray-500">April 16, 2020</p> */}
+    <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
+    <a href="#" className="text-gray-900 hover:text-purple-700">UF Health Orthocare After Hours</a>
+    <p className="mb-4 text-base font-normal text-gray-600">
+      1600 SW Archer Rd.
+      Gainesville FL 32608
+      352-273-7329
+     tinyurl.com/orthohours
+    </p> 
+    </h2> 
+  </div>
     </div>
+{/* <div className="flex flex-col items-center justify-center pt-12 mt-12 space-x-0 space-y-2 border-t border-gray-200 md:space-x-2 md:space-y-0 md:flex-row">
+  <a href="#" className="w-full rounded-full btn btn-light btn-xl md:w-auto">Previous Page</a>
+  <a href="#" className="w-full rounded-full btn btn-light btn-xl md:w-auto">Next Page</a>
+</div> */}
+</section>
+      
+  </div>
     )
 }
