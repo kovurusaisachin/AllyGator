f40ec1549d340e59c0865586b447fee561532600
Maitreyi S <maitreyis@Maitreyis-MacBook-Pro-2.local>
2022-03-04 02:42:58 -0500

    Off-campus safety page 1 done

--- Frontend/allygator_frontend/pages/dashboard/miscellaneous/offcampussafety/index.tsx
+++ Frontend/allygator_frontend/pages/dashboard/miscellaneous/offcampussafety/index.tsx
@@ -5,9 +5,52 @@ export default function offcampussafety () {
         <Sidebar />
         <section className="w-full px-4 py-24 mx-auto max-w-7xl md:w-3/4 lg:w-2/4">
   <div className="mb-12 text-left md:text-center">
-    <h2 className="mb-2 text-3xl font-extrabold leading-tight text-gray-900">Off campus safety</h2>
-    <p className="text-lg text-gray-500">Off campus safety from the dean of students office.Gainesville is considered a safe community, but it is not immune from criminal activity. However, common sense precautions can reduce your vulnerability to both personal and property crimes.</p>
+    <h2 className="mb-2 text-3xl font-extrabold leading-tight text-gray-900">
+    Off campus safety
+    </h2>
+    <p className="text-lg text-gray-500">
+     Off campus safety from the dean of students office.Gainesville is considered a safe community, but it is not immune from criminal activity. However, common sense precautions can reduce your vulnerability to both personal and property crimes.
+    </p>
   </div>
-  
+
+  <div>
+      {/* <p className="pt-12 mb-3 text-sm font-normal text-gray-500">April 16, 2020</p> */}
+      <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
+        <a href="#" className="text-gray-900 hover:text-purple-700">Personal safety tips</a>
+      </h2>
+      <p className="mb-4 text-base font-normal text-gray-600">
+        1. Call 911 if there is an emergency
+      </p>
+      <p className="mb-4 text-base font-normal text-gray-600">
+        2. Get to know your neighbors
+      </p>
+      <p className="mb-4 text-base font-normal text-gray-600">
+        3. Don't advertise your class or work schedule
+      </p>
+      <p className="mb-4 text-base font-normal text-gray-600">
+        4. Close shades or drapes after dark
+      </p>
+      <p className="mb-4 text-base font-normal text-gray-600">
+        5. Look through a peephole or window before opening the door
+      </p>
+      <p className="mb-4 text-base font-normal text-gray-600">
+       6. Never let strangers in the door. If it is a repair or service representative, call the property owner or company to ensure they are supposed to be there. Always insist on identification.
+      </p>
+      <p className="mb-4 text-base font-normal text-gray-600">
+       7. Have your keys ready in your hand as you approach your car or entryway.
+       </p>
+      <p className="mb-4 text-base font-normal text-gray-600">
+        8. Immediately report malfunctioning doors, windows, security gratings, lights, overgrown shrubbery, etc. to the person responsible for maintaining your residence.
+      </p>
+      <p className="mb-4 text-base font-normal text-gray-600">
+       9. Avoid walking alone at night, but if you must, stay in well-lit, open areas.
+      </p>
+      <p className="mb-4 text-base font-normal text-gray-600">
+      10. Stay sober and coherent.
+      </p>
+      {/* <a href="#" className="btn btn-light btn-sm">Continue Reading</a> */}
+    </div>
+  </section>
+  </div>
     )
 }
