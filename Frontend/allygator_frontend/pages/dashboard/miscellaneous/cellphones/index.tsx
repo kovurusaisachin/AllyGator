40a92fd716b890906b31b3c182f4d323fbfc7fa1
Sai Sachin Kovuru <saisachinkovuru@Sais-MacBook-Pro.local>
2022-03-04 00:22:58 -0500

    Adding cell phone information page

--- Frontend/allygator_frontend/pages/dashboard/miscellaneous/cellphones/index.tsx
+++ Frontend/allygator_frontend/pages/dashboard/miscellaneous/cellphones/index.tsx
@@ -3,8 +3,133 @@ export default function cellphones () {
     return (
         <div className="flex flex-col-2">
         <Sidebar />
-        <h2>More Content Coming Soon!!!</h2>
+        <section className="w-full px-4 py-24 mx-auto max-w-7xl md:w-3/4 lg:w-2/4">
+  <div className="mb-12 text-left md:text-center">
+    <h2 className="mb-2 text-3xl font-extrabold leading-tight text-gray-900">Cell phones</h2>
+    <p className="text-lg text-gray-500">Directory of cell phone providers.</p>
+  </div>
+  <div className="flex flex-col space-y-12 divide-y divide-gray-200">
+    <div>
+      {/* <p className="pt-12 mb-3 text-sm font-normal text-gray-500">April 16, 2020</p> */}
+      <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
+        <a href="#" className="text-gray-900 hover:text-purple-700">AT&T</a>
+      </h2>
+      <p className="mb-4 text-base font-normal text-gray-600">
+        3626 SW Archer Rd.
+        Gainesville FL 32608
+      </p>
+      <p className="mb-4 text-base font-normal text-gray-600">
+       6110 NW 4 Pl. 
+       Gainesville FL 32607
+      </p>
+      <p className="mb-4 text-base font-normal text-gray-600">
+      3720 NW 13th St. 
+      Gainesville FL 32609
+      </p>
+    </div>
+    <div>
+      {/* <p className="pt-12 mb-3 text-sm font-normal text-gray-500">April 16, 2020</p> */}
+      <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
+        <a href="#" className="text-gray-900 hover:text-purple-700">Boost Mobile</a>
+      </h2>
+      <p className="mb-4 text-base font-normal text-gray-600">
+        3439 W University Ave.
+        Gainesville FL 32607
+      </p>
+      <p className="mb-4 text-base font-normal text-gray-600">
+        2302 E University Ave.
+        Gainesville FL
+      </p>
+      <p className="mb-4 text-base font-normal text-gray-600">
+        6419 Newberry Rd.
+        Gainesville FL 32605
+      </p>
+      <p className="mb-4 text-base font-normal text-gray-600">
+        424 NW 13th St.
+        Gainesville FL 32601
+      </p>
+      <p className="mb-4 text-base font-normal text-gray-600">
+        2448 N Main St.
+        Gainesville FL 32609
+      </p>
+      <p className="mb-4 text-base font-normal text-gray-600">
+       7349 W Newberry Rd.
+       Gainesville  FL 32605
+      </p>
+      <p className="mb-4 text-base font-normal text-gray-600">
+        5212 SW 34th St.
+        Gainesville FL 32608
+      </p>
+      <p className="mb-4 text-base font-normal text-gray-600">
+        6250 NW 23rd St.
+        Gainesville FL 32653
+      </p>
+      {/* <a href="#" className="btn btn-light btn-sm">Continue Reading</a> */}
+    </div>
+    <div>
+      {/* <p className="pt-12 mb-3 text-sm font-normal text-gray-500">April 16, 2020</p> */}
+      <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
+        <a href="#" className="text-gray-900 hover:text-purple-700">Cricket Wireless</a>
+      </h2>
+      <p className="mb-4 text-base font-normal text-gray-600">
+        405 NW 13th St.
+        Gainesville FL 32601
+      </p>
+      <p className="mb-4 text-base font-normal text-gray-600">
+        2803 SW 42nd St. #6
+        Gainesville FL 32608
+      </p>
+      <p className="mb-4 text-base font-normal text-gray-600">
+        1140 NW 76th Blvd #16A
+        Gainesville FL 32606
+      </p>
+      {/* <a href="#" className="btn btn-light btn-sm">Continue Reading</a> */}
+    </div>
+    <div>
+      {/* <p className="pt-12 mb-3 text-sm font-normal text-gray-500">April 16, 2020</p> */}
+      <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
+        <a href="#" className="text-gray-900 hover:text-purple-700">Sprint</a>
+      </h2>
+      <p className="mb-4 text-base font-normal text-gray-600">
+        3600 SW Archer Rd. Ste. C.
+        Gainesville FL 32608
+      </p>
+      <p className="mb-4 text-base font-normal text-gray-600">
+        5171 NW 43rd St.
+        Gainesville FL 32606
+      </p>
+      <p className="mb-4 text-base font-normal text-gray-600">
+       2 NW 16th Ave.
+       Gainesville FL 32601
+      </p>
+      {/* <a href="#" className="btn btn-light btn-sm">Continue Reading</a> */}
+    </div>
+    <div>
+      {/* <p className="pt-12 mb-3 text-sm font-normal text-gray-500">April 16, 2020</p> */}
+      <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
+      <a href="#" className="text-gray-900 hover:text-purple-700">T-Mobile</a>
+      <p className="mb-4 text-base font-normal text-gray-600">
+        3401 SW Archer Rd.
+        Gainesville  FL 32608
+      </p>
+      <p className="mb-4 text-base font-normal text-gray-600">
+       6921 W Newberry Rd, Ste. C.
+       Gainesville FL 32605
+      </p>
+      <p className="mb-4 text-base font-normal text-gray-600">
+       2612 NW 13th St. Verizon Authorized Retailer 
+       Gainesville FL 32609
+      </p>
         
+      </h2>
+      
+    </div>
+      </div>
+  {/* <div className="flex flex-col items-center justify-center pt-12 mt-12 space-x-0 space-y-2 border-t border-gray-200 md:space-x-2 md:space-y-0 md:flex-row">
+    <a href="#" className="w-full rounded-full btn btn-light btn-xl md:w-auto">Previous Page</a>
+    <a href="#" className="w-full rounded-full btn btn-light btn-xl md:w-auto">Next Page</a>
+  </div> */}
+</section>
         
     </div>
     )
