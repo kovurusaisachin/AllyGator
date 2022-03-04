6bb7bd2ffd757e5929379bc692e7f8a2893c52bf
prashantkapri7@gmail.com <prashantkapri7@gmail.com>
2022-03-04 18:57:25 -0500

    dashboard page test done

--- Frontend/allygator_frontend/cypress/integration/dashboard.spec.js
+++ Frontend/allygator_frontend/cypress/integration/dashboard.spec.js
@@ -1,31 +1,61 @@
 /// <reference types="cypress"/>
 
-context("Dashboard Page", ()=>{
-    beforeEach(()=>{
-        cy.visit("http://localhost:3000/");
+context("Dashboard Page", () => {
+    beforeEach(() => {
+      cy.visit("http://localhost:3000/dashboard");
+    });
+  
+    it("Checks if every component on sidebar is loaded and its navigation is working", () => {
+      //Check if header is present
+      cy.get("button").contains("AllyGators");
+  
+      // forum page
+      cy.contains("Forum").click();
+      cy.location("pathname").should("eq", "/dashboard/forum");
+      cy.go("back");
+
+      //connections page
+      cy.contains("Connections").click();
+      cy.location("pathname").should("eq", "/dashboard/connection");
+      cy.go("back");
+      
+      //chat page
+      cy.contains("Chat").click();
+      cy.location("pathname").should("eq", "/dashboard/chat");
+      cy.go("back");
+
+      //miscellaneous page
+      cy.contains("Miscellaneous").click();
+      cy.location("pathname").should("eq", "/dashboard/miscellaneous");
+      cy.go("back");
+
+      //profile page
+      cy.contains("Profile").click();
+      cy.location("pathname").should("eq", "/dashboard/profile");
+      cy.go("back");
+    });
+  
+    it("Check if every component of analytics card is working or not",() => {
+        //Check if header is present
+        cy.get("h1").contains("Dashboard");
+        cy.contains("Connections");
+        cy.contains("Posts");
+        cy.contains("Activities");
+        cy.contains("Pending connections");
+
     });
 
-    it("Checks if every component is loaded", ()=>{
+    it("Check if table on dashboard is loaded or not",() => {
+        //check connection table
+        cy.contains("Connection's list");
+        cy.get("thead").should("contain","Name").and("contain","Major").and("contain","Department").and("contain","Nationality").and("contain","Status");
 
-        //Check if sidebar is present
-        cy.get(".mx-2 text-lg font-semibold text-gray-800 dark:text-white").contains("AllyGators");
+        //check course list table
+        cy.contains("Top Course list");
+        cy.get("thead").should("contain","Name").and("contain","Course").and("contain","Department").and("contain","Faculty").and("contain","Likes");
 
-        //Check if Navbar has Menu
-        cy.get(".chakra-menu__menu-button").click().get(".chakra-menu__menu-list").should('contain', "Home").and("contain","Search").and("contain",  "Buy Property").and("contain",  "Rent Property");
-        
-        //Check if Navbar has SignIn and SignUp
-        cy.get(".chakra-button").should("contain", "Sign In").and("contain", "Sign Up");
-        
-        //Check if 6 Rent-able properties loaded
-        cy.get("p").contains("RENT A HOME");
-        cy.get(".rent").children("div").should("have.length", 6);
-
-        //Check if 6 Sale-able properties loaded
-        cy.get("p").contains("BUY A HOME");
-        cy.get(".sale").children("div").should("have.length", 6);
     });
-
-
+  
     
-
-}) 
+  });
+  
