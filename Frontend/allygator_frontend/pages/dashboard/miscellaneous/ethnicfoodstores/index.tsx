e6be08b50208722491a3c11e1f10ed621edb56bc
Maitreyi S <maitreyis@Maitreyis-MacBook-Pro-2.local>
2022-03-04 01:55:10 -0500

    Ethnic food stores page 2 modified

--- Frontend/allygator_frontend/pages/dashboard/miscellaneous/ethnicfoodstores/index.tsx
+++ Frontend/allygator_frontend/pages/dashboard/miscellaneous/ethnicfoodstores/index.tsx
@@ -6,7 +6,7 @@ export default function ethnicfoodstores () {
         <section className="w-full px-4 py-24 mx-auto max-w-7xl md:w-3/4 lg:w-2/4">
   <div className="mb-12 text-left md:text-center">
     <h2 className="mb-2 text-3xl font-extrabold leading-tight text-gray-900">Ethnic Food Stores</h2>
-    <p className="text-lg text-gray-500">Collection of ethnic food stores in Gainesville.</p>
+    <p className="text-lg text-gray-500">Most local markets, including farmers markets, and supermarkets have ethnic food sections, with selection varying by location.</p>
   </div>
   <div className="flex flex-col space-y-12 divide-y divide-gray-200">
     <div>
