255b2e7b7564e57f4d104406bc5043a1fcdced92
Maitreyi S <maitreyis@Maitreyis-MacBook-Pro-2.local>
2022-03-04 02:57:51 -0500

    Off campus safety page 2 done

--- Frontend/allygator_frontend/pages/dashboard/miscellaneous/offcampussafety/index.tsx
+++ Frontend/allygator_frontend/pages/dashboard/miscellaneous/offcampussafety/index.tsx
@@ -50,6 +50,38 @@ export default function offcampussafety () {
       </p>
       {/* <a href="#" className="btn btn-light btn-sm">Continue Reading</a> */}
     </div>
+
+    <div>
+      {/* <p className="pt-12 mb-3 text-sm font-normal text-gray-500">April 16, 2020</p> */}
+      <h2 className="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
+        <a href="#" className="text-gray-900 hover:text-purple-700">Protecting your property</a>
+      </h2>
+      <p className="mb-4 text-base font-normal text-gray-600">
+        1. Buy renter's insurance
+      </p>
+      <p className="mb-4 text-base font-normal text-gray-600">
+        2. Keep your outside doors locked even when you are home.
+      </p>
+      <p className="mb-4 text-base font-normal text-gray-600">
+        3. Be sure your locks are strong and working.
+      </p>
+      <p className="mb-4 text-base font-normal text-gray-600">
+        4. Permanently mark valuables and record serial number.
+      </p>
+      <p className="mb-4 text-base font-normal text-gray-600">
+        5. Leave some light on when you are not home.
+      </p>
+      <p className="mb-4 text-base font-normal text-gray-600">
+       6. Do not leave valuables such as computers or purses when they can be seen from a window.
+      </p>
+      <p className="mb-4 text-base font-normal text-gray-600">
+       7. Do not leave a note on the door saying when you will return.
+       </p>
+      <p className="mb-4 text-base font-normal text-gray-600">
+        8. If you own a bike and it must be stored outside, make sure it is secured to an immovable object with a high quality locking device.
+      </p>
+     
+    </div>
   </section>
   </div>
     )
