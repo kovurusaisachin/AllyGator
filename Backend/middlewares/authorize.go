7e0748672da99094b181db402b0666088e1d59f4
Sai Sachin Kovuru <saisachinkovuru@Sais-MacBook-Pro.local>
2022-03-16 18:05:22 -0400

    Setting up an authorization middleware #91

---
+++ Backend/middlewares/authorize.go
@@ -0,0 +1,49 @@
+package middlewares
+
+import (
+	"fmt"
+	"strings"
+
+	"allygator.com/gatorweb/auth"
+	"github.com/gin-gonic/gin"
+)
+
+// Authz validates token and authorizes users
+func Authz() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		clientToken := c.Request.Header.Get("Authorization")
+		if clientToken == "" {
+			c.JSON(403, "No Authorization header provided")
+			c.Abort()
+			return
+		}
+
+		extractedToken := strings.Split(clientToken, "Bearer ")
+
+		if len(extractedToken) == 2 {
+			clientToken = strings.TrimSpace(extractedToken[1])
+		} else {
+			c.JSON(400, "Incorrect Format of Authorization Token")
+			c.Abort()
+			return
+		}
+
+		jwtWrapper := auth.JwtWrapper{
+			SecretKey: "verysecretkey",
+			Issuer:    "AuthService",
+		}
+
+		claims, err := jwtWrapper.ValidateToken(clientToken)
+		if err != nil {
+			c.JSON(401, err.Error())
+			c.Abort()
+			return
+		}
+
+		c.Set("email", claims.Email)
+		fmt.Println("claimsemail", claims.Email)
+
+		c.Next()
+
+	}
+}
