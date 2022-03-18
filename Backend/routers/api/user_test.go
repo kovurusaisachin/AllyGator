739da3fda5c2e79936c5c4aec3ec7f743507d10e
Maitreyi S <maitreyis@Maitreyis-MacBook-Pro-2.local>
2022-03-18 18:22:05 -0400

    Create an api to Get Users Information by Email-ID with GO-Unit-test code #95

--- Backend/routers/api/user_test.go
+++ Backend/routers/api/user_test.go
@@ -254,6 +254,68 @@ func TestGetUserById(t *testing.T) {
 
 }
 
+//getUserByEmail API Unit-test
+func TestGetUserByEmail(t *testing.T) {
+	err := models.ConnectDatabase()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Switching to test mode so we don't get such noisy output
+	gin.SetMode(gin.TestMode)
+
+	// Settingup the router, and
+	// registering the routes
+	r := gin.Default()
+	v1 := r.Group("/api/v1")
+	v1.GET("mail/:id", GetUserByEmail)
+
+	t.Run("Incorrect User EmailID", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodGet, "/api/v1/user/kovuru.sachin@ufl.edu", nil)
+		if err != nil {
+			t.Fatalf("Couldn't create request: %v\n", err)
+		}
+
+		// Creating a response recorder so that we can inspect the response
+		w := httptest.NewRecorder()
+
+		// Performing the request
+		fmt.Print("\nMock API for getUserByEmail - Sending the incorrect User Email ID\n")
+		r.ServeHTTP(w, req)
+		fmt.Print("\n\n")
+		fmt.Println(w.Body)
+		fmt.Print("\n\n")
+		// Checking if the response was what we expected
+		if w.Code == http.StatusOK {
+			t.Logf("Expected to get status %d is same ast %d\n", http.StatusOK, w.Code)
+		} else {
+			t.Logf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
+		}
+	})
+	t.Run("Correct User EmailID", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodGet, "/api/v1/user/kovuru.saisachin@ufl.edu", nil)
+		if err != nil {
+			t.Fatalf("Couldn't create request: %v\n", err)
+		}
+
+		// Creating a response recorder so that we can inspect the response
+		w := httptest.NewRecorder()
+
+		// Performing the request
+		fmt.Print("\nMock API for getUserByEMail - Sending correct user Email-ID\n")
+		r.ServeHTTP(w, req)
+		fmt.Print("\n\n")
+		fmt.Println(w.Body)
+		fmt.Print("\n\n")
+		// Checking if the response was what we expected
+		if w.Code == http.StatusOK {
+			t.Logf("Expected to get status %d is same ast %d\n", http.StatusOK, w.Code)
+		} else {
+			t.Logf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
+		}
+	})
+
+}
+
 //UpdateUser API Unit-test
 func TestUpdateUser(t *testing.T) {
 	err := models.ConnectDatabase()
