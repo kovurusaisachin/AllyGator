d2a695a191f52e3723942d2672d014e05df47653
Noopur Thanvi <noopurthanvi@Noopurs-MacBook-Air.local>
2022-03-21 10:40:52 -0400

    faculty search filter added

--- Frontend/allygator_frontend/pages/dashboard/index.tsx
+++ Frontend/allygator_frontend/pages/dashboard/index.tsx
@@ -25,8 +25,10 @@ export default function dashboard() {
     courseData: [],
     courseResult:[],
     facultyData:[],
+    facultyResult:[],
     query: {
       searchText: "",
+      searchTextF: "",
       searchTextC: "",
       status: "active",
       department: "",
@@ -67,7 +69,8 @@ export default function dashboard() {
           chatData: chat?.data,
           courseData: course?.data,
           courseResult: course?.data,
-          facultyData: faculty?.data
+          facultyData: faculty?.data,
+          facultyResult: faculty?.data
         });
       }
     );
@@ -138,7 +141,28 @@ export default function dashboard() {
     state.query.searchTextC
     // state?.userData
   ]);
-  console.log(state?.result, "prashant");
+  useEffect(() => {
+    const newResults = state?.facultyData?.filter(
+      (product) =>
+        product?.info
+          ?.toLowerCase()
+          ?.includes(state.query.searchTextF?.toLowerCase()) ||
+        product?.facultyname
+          ?.toLowerCase()
+          ?.includes(state.query.searchTextF?.toLowerCase()) ||
+        product?.deptName
+          ?.toLowerCase()
+          ?.includes(state.query.searchTextF?.toLowerCase()) 
+    );
+
+    setState({
+      ...state,
+      facultyResult: newResults,
+    });
+  }, [
+    state.query.searchTextF
+    // state?.userData
+  ]);
 
   const tableHeader = [
     { name: "Name", href: "#home" },
@@ -402,7 +426,46 @@ export default function dashboard() {
             <h3 className="text-2xl font-bold my-2 leading-7 text-gray-900 sm:leading-9 sm:truncate">
               Faculty list
             </h3>
-              <FacultyTable header={facultyHeader} data={state?.facultyData} />
+            <div className="col-span-1">
+                <div className="w-full">
+                  <label className="block text-base font-semibold text-gray-900">
+                    Search
+                  </label>
+                  <div className="relative mt-1.5 mb-5">
+                    <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                      <svg
+                        className="h-5 w-5 text-gray-400"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </div>
+                    <input
+                      className="block md:text-sm w-full pl-10 pr-3 py-2 border-2 border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-primary-500 focus:border-primary-500 sm:text-lg"
+                      placeholder="Search by Faculty name or Department"
+                      type="text"
+                      value={state.query.searchTextF}
+                      onChange={(e) => {
+                        setState({
+                          ...state,
+                          query: {
+                            ...state.query,
+                            searchTextF: e.target.value,
+                          },
+                        });
+                      }}
+                    />
+                  </div>
+                </div>
+              </div>
+              <FacultyTable header={facultyHeader} data={state?.facultyResult} />
             </div>
           </div>
         </div>
