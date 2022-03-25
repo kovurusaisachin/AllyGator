63f4652165962a925967d69f7335ae011344547e
prashantkapri7@gmail.com <prashantkapri7@gmail.com>
2022-03-25 16:29:15 -0400

    rmp link svg

--- Frontend/allygator_frontend/components/Table/facultyTable.tsx
+++ Frontend/allygator_frontend/components/Table/facultyTable.tsx
@@ -38,7 +38,17 @@ export default function Table(props) {
                       </div>
                     </td>
                     
-                    <td className="px-3 py-4 whitespace-nowrap text-sm text-gray-500">{person?.info}</td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                    <Link href={person?.info ?? ""}>
+                          <a target="_blank">
+                          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+  <path d="M12 14l9-5-9-5-9 5 9 5z" />
+  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+<path strokeLinecap="round" strokeLinejoin="round" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
+</svg>
+                          </a>
+                        </Link>      
+                </td>
                     
                   </tr>
                 ))}
