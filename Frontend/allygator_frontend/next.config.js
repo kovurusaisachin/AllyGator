699529c0abcba488803ff36dbb3ece01a945fc0a
prashantkapri7@gmail.com <prashantkapri7@gmail.com>
2022-03-31 01:12:42 -0400

    config updates

--- Frontend/allygator_frontend/next.config.js
+++ Frontend/allygator_frontend/next.config.js
@@ -1,27 +1,27 @@
 
 module.exports = ({
-  reactStrictMode: true,
-  images: {
-    disableStaticImages: true
-  },
-  webpack: (config, options) => {
-      const { isServer } = options;
-      config.module.rules.push({
-          test: /\.(ogg|mp3|wav|mpe?g|png|jpe?g|gif|svg)$/i,
-          exclude: config.exclude,
-          use: [
-              {
-                  loader: require.resolve('file-loader'),
-                  options: {
-                      limit: config.inlineImageLimit,
-                      publicPath: `_next/static/images/`,
-                      outputPath: `${isServer ? '../' : ''}static/images/`,
-                      name: '[name]-[hash].[ext]',
-                      esModule: config.esModule || false,
-                  },
-              },
-          ],
-      });
-      return config
-  },
-})
+	reactStrictMode: true,
+	images: {
+	  disableStaticImages: true
+	},
+	webpack: (config, options) => {
+		const { isServer } = options;
+		config.module.rules.push({
+			test: /\.(ogg|mp3|wav|mpe?g|png|jpe?g|gif|svg)$/i,
+			exclude: config.exclude,
+			use: [
+				{
+					loader: require.resolve('file-loader'),
+					options: {
+						limit: config.inlineImageLimit,
+						publicPath: `_next/static/images/`,
+						outputPath: `${isServer ? '../' : ''}static/images/`,
+						name: '[name]-[hash].[ext]',
+						esModule: config.esModule || false,
+					},
+				},
+			],
+		});
+		return config
+	},
+  })
