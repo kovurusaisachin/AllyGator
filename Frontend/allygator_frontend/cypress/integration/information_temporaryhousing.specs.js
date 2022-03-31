49764eb44916ba837811863bd84cfb338bcfb241
Maitreyi S <maitreyis@Maitreyis-MacBook-Pro-2.local>
2022-03-31 01:33:16 -0400

    Cypress testing for temporary housing page #122

---
+++ Frontend/allygator_frontend/cypress/integration/information_temporaryhousing.specs.js
@@ -0,0 +1,31 @@
+it('Successful login', function () {
+        
+    cy.visit('http://localhost:3000/login')
+    cy.get("[data-cy='login-email-input']")
+    .type("maitreyi.srinivas@ufl.edu")
+    cy.get("[data-cy='login-password-input']")
+    .type("maitreyi")
+    cy.contains("Login")
+    .click()
+    cy.contains("Login successfull")
+    .click()
+})
+
+it('Testing Off campus safety page navigation',function(){
+    cy.visit('http://localhost:3000/dashboard')
+    cy.contains("Information").click();
+    cy.location("pathname").should("eq", "/dashboard/miscellaneous");
+    cy.contains("Temporary Housing").click();
+    cy.location("pathname").should("eq", "/dashboard/miscellaneous/temporaryhousing");
+
+}) 
+
+it('Testing Temporary Housing Information',function(){
+    cy.visit('http://localhost:3000/dashboard/miscellaneous/temporaryhousing')
+    cy.contains("Temporary Housing");
+}) 
+
+it('Successful logout', function () {
+    
+    cy.contains("Logout").click()
+})
