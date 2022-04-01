604991ed3f7f555065b8a9917116c61989aa64fb
Sai Sachin Kovuru <saisachinkovuru@Sais-MacBook-Pro.local>
2022-03-31 23:20:38 -0400

    Cypress testing for Electricity Page Navigation #125

--- Frontend/allygator_frontend/cypress/integration/information_electricity.spec.js
+++ Frontend/allygator_frontend/cypress/integration/information_electricity.spec.js
@@ -25,14 +25,31 @@ describe('Login Tests', function () {
         cy.location("pathname").should("eq", "/dashboard/miscellaneous/electricity");
 
     }) 
+
+    it('Testing Elcetricity Help',function(){
+        cy.visit('http://localhost:3000/dashboard/miscellaneous/electricity')
+        cy.contains("Electricity Help");
+    }) 
+    it('Testing GRU Office Details',function(){
+        cy.visit('http://localhost:3000/dashboard/miscellaneous/electricity')
+        cy.contains("GRU Office Details");
+       
+
+    }) 
+    it('Testing GRU Downtown information',function(){
+        cy.visit('http://localhost:3000/dashboard/miscellaneous/electricity')
+        cy.contains("Downtown Office");
+    }) 
+    it('Testing Start Services information',function(){
+        cy.visit('http://localhost:3000/dashboard/miscellaneous/electricity')
+        cy.contains("Start Services");
+    }) 
+    it('Testing Tax Identification Number Information',function(){
+        cy.visit('http://localhost:3000/dashboard/miscellaneous/electricity')
+        cy.contains("If you do not have a SSN or Tax Identification Number, then you must go into the GRU office downtown to set up your account");
+    }) 
     it('Successful logout', function () {
         
         cy.contains("Logout").click()
     })
 })
-   
-    
-
-    
-
-    
