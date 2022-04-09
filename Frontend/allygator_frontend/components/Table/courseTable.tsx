56e5af62acab04463b0175818b939e6e27f28099
prashantkapri7@gmail.com <prashantkapri7@gmail.com>
2022-04-09 19:32:06 -0400

    pagination added to faculty table

--- Frontend/allygator_frontend/components/Table/courseTable.tsx
+++ Frontend/allygator_frontend/components/Table/courseTable.tsx
@@ -1,7 +1,6 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Link from "next/link";
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid'
-
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/solid";
 
 export default function Table(props) {
   console.log(props, "pk");
@@ -16,7 +15,7 @@ export default function Table(props) {
   const paginateFront = () => setCurrentPage(currentPage + 1);
   const paginateBack = () => setCurrentPage(currentPage - 1);
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
-  const totalPosts = props?.data?.length
+  const totalPosts = props?.data?.length;
   const pageNumbers = [];
 
   for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
@@ -48,7 +47,10 @@ export default function Table(props) {
                     <Header array={props.header} />
                   </tr>
                 </thead>
-                <tbody className="bg-white divide-y divide-gray-200 overflow-y-scroll w-full" style={{height:'10px'}}>
+                <tbody
+                  className="bg-white divide-y divide-gray-200 overflow-y-scroll w-full"
+                  style={{ height: "10px" }}
+                >
                   {currentPosts?.map((person) => (
                     <tr key={person.coursename}>
                       <td className="px-3 py-4 whitespace-nowrap">
@@ -77,80 +79,96 @@ export default function Table(props) {
                 </tbody>
               </table>
               <div className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
-      <div className="flex-1 flex justify-between sm:hidden">
-        <a
-          onClick={() => {
-            paginateBack();
-          }}
-          href="#"
-          className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
-        >
-          Previous
-        </a>
-        <a
-        onClick={() => {
-          paginateFront();
-        }}
-          href="#"
-          className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
-        >
-          Next
-        </a>
-      </div>
-      <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
-        <div>
-          <p className="text-sm text-gray-700">
-            Showing <span className="font-medium">{currentPage * postsPerPage - 5}</span> to <span className="font-medium">{currentPage * postsPerPage}</span> of{' '}
-            <span className="font-medium">{totalPosts}</span> results
-          </p>
-        </div>
-        <div>
-          <nav className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px" aria-label="Pagination">
-            
-            <a
-            onClick={() => {
-              paginateBack();
-            }}
-              href="#"
-              className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
-            >
-              <span className="sr-only">Previous</span>
-              <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
-            </a>
-            {/* Current: "z-10 bg-indigo-50 border-indigo-500 text-indigo-600", Default: "bg-white border-gray-300 text-gray-500 hover:bg-gray-50" */}
-            <ul className='flex pl-0 rounded list-none flex-wrap'>
-          <li>
-            {pageNumbers.map((number) => (
-              <a
-                onClick={() => {
-                  paginate(number);
-                }}
-                href='#'
-                className={
-                  currentPage === number
-                    ? "bg-blue border-red-300 text-red-500 hover:bg-blue-200 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
-                    : "bg-white border-gray-300 text-gray-500 hover:bg-blue-200 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
-                }
-              >
-                {number}
-              </a>
-            ))}
-          </li>
-          </ul>
-            <a
-            onClick={() => {
-              paginateFront();
-            }}
-              href="#"
-              className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
-            >
-              <span className="sr-only">Next</span>
-              <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
-            </a>
-          </nav>
-        </div>
-      </div>
-    </div>
+                <div className="flex-1 flex justify-between sm:hidden">
+                  <a
+                    onClick={() => {
+                      paginateBack();
+                    }}
+                    href="#"
+                    className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+                  >
+                    Previous
+                  </a>
+                  <a
+                    onClick={() => {
+                      paginateFront();
+                    }}
+                    href="#"
+                    className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+                  >
+                    Next
+                  </a>
+                </div>
+                <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+                  <div>
+                    <p className="text-sm text-gray-700">
+                      Showing{" "}
+                      <span className="font-medium">
+                        {currentPage * postsPerPage - 4}
+                      </span>{" "}
+                      to{" "}
+                      <span className="font-medium">
+                        {currentPage * postsPerPage}
+                      </span>{" "}
+                      of <span className="font-medium">{totalPosts}</span>{" "}
+                      results
+                    </p>
+                  </div>
+                  <div>
+                    <nav
+                      className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px"
+                      aria-label="Pagination"
+                    >
+                      <a
+                        onClick={() => {
+                          paginateBack();
+                        }}
+                        href="#"
+                        className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                      >
+                        <span className="sr-only">Previous</span>
+                        <ChevronLeftIcon
+                          className="h-5 w-5"
+                          aria-hidden="true"
+                        />
+                      </a>
+                      {/* Current: "z-10 bg-indigo-50 border-indigo-500 text-indigo-600", Default: "bg-white border-gray-300 text-gray-500 hover:bg-gray-50" */}
+                      <ul className="flex pl-0 rounded list-none flex-wrap">
+                        <li>
+                          {pageNumbers.map((number) => (
+                            <a
+                              onClick={() => {
+                                paginate(number);
+                              }}
+                              href="#"
+                              className={
+                                currentPage === number
+                                  ? "bg-blue border-red-300 text-red-500 hover:bg-blue-200 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
+                                  : "bg-white border-gray-300 text-gray-500 hover:bg-blue-200 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
+                              }
+                            >
+                              {number}
+                            </a>
+                          ))}
+                        </li>
+                      </ul>
+                      <a
+                        onClick={() => {
+                          paginateFront();
+                        }}
+                        href="#"
+                        className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+                      >
+                        <span className="sr-only">Next</span>
+                        <ChevronRightIcon
+                          className="h-5 w-5"
+                          aria-hidden="true"
+                        />
+                      </a>
+                    </nav>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
