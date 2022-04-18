fdc7a36d90a036703626de3c8f873b3270c293e4
Sai Sachin Kovuru <saisachinkovuru@Sais-MacBook-Pro.local>
2022-04-18 11:55:02 -0400

    Adding Go-Unit-test cases for GetPostsByUserId Forum API developed in Sprint-4 #155

--- Backend/routers/api/post_test.go
+++ Backend/routers/api/post_test.go
@@ -143,3 +143,65 @@ func TestGetPosts(t *testing.T) {
 		}
 	})
 }
+
+//GetPostsByUserId API Unit-test
+func TestGetPostsByUserId(t *testing.T) {
+	err := models.ConnectDatabase()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Switching to test mode so we don't get such noisy output
+	gin.SetMode(gin.TestMode)
+
+	// Settingup the router, and
+	// registering the routes
+	r := gin.Default()
+	v1 := r.Group("/api/v1")
+	v1.GET("post/user/:id", GetPostsByUserId)
+
+	t.Run("Incorrect UserID", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodGet, "/api/v1/post/user/111", nil)
+		if err != nil {
+			t.Fatalf("Couldn't create request: %v\n", err)
+		}
+
+		// Creating a response recorder so that we can inspect the response
+		w := httptest.NewRecorder()
+
+		// Performing the request
+		fmt.Print("\nMock API for getPostsByUserId - Sending the incorrect UserID\n")
+		r.ServeHTTP(w, req)
+		fmt.Print("\n\n")
+		fmt.Println(w.Body)
+		fmt.Print("\n\n")
+		// Checking if the response was what we expected
+		if w.Code == http.StatusOK {
+			t.Logf("Expected to get status %d is same ast %d\n", http.StatusOK, w.Code)
+		} else {
+			t.Logf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
+		}
+	})
+	t.Run("Correct UserID", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodGet, "/api/v1/post/user/1", nil)
+		if err != nil {
+			t.Fatalf("Couldn't create request: %v\n", err)
+		}
+
+		// Creating a response recorder so that we can inspect the response
+		w := httptest.NewRecorder()
+
+		// Performing the request
+		fmt.Print("\nMock API for getPostsByUserId - Sending correct UserID\n")
+		r.ServeHTTP(w, req)
+		fmt.Print("\n\n")
+		fmt.Println(w.Body)
+		fmt.Print("\n\n")
+		// Checking if the response was what we expected
+		if w.Code == http.StatusOK {
+			t.Logf("Expected to get status %d is same ast %d\n", http.StatusOK, w.Code)
+		} else {
+			t.Logf("Expected to get status %d but instead got %d\n", http.StatusOK, w.Code)
+		}
+	})
+
+}
