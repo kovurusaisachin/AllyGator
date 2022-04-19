19216ca9c82dfce9202429d759ef0f06b73dbb47
prashantkapri7@gmail.com <prashantkapri7@gmail.com>
2022-04-19 11:10:12 -0400

    cometchat const key updated

--- Frontend/allygator_frontend/components/constant/index.tsx
+++ Frontend/allygator_frontend/components/constant/index.tsx
@@ -1,7 +1,7 @@
 export const API_URL = 'http://localhost:8080/api/v1'  
 export const COMETCHAT_CONSTANTS = {
-    APP_ID: "205658ba176301cd",
+    APP_ID: "207645a8cb0e8782",
     REGION: "us",
-    AUTH_KEY: "a2272caadec7674823861d157fc3180e62422028",
-    REST_API_KEY: "b22df5dcbfb8054844f6a6e3668a490972d2a617"
+    AUTH_KEY: "ab6dd59cbe35e0fd9409b966382acab40c66e2be",
+    REST_API_KEY: "c836d6b897d4e1c99dd6bd43c9bf8a45469df0da"
   }
